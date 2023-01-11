ac1f400b38f5cd22ead2e985762a5212298337ff
JonasChen <tom200425@gmail.com>
2023-01-11 15:01:21 +0800

    新增筆記 MarkDown語法

--- src/components/myp102/blogMenu.js
+++ src/components/myp102/blogMenu.js
@@ -2,7 +2,7 @@ export const blogMenu = [
     {
         "meta":{
             "id": 1,
-            "label": "文章測試1",
+            "label": "MarkDown語法大全",
             "name": "blog-1", //檔名
         },
     },
